0071ee8b297ed9a27e902036037c28b18b25788b
Cutina Woof <gretchenista@gmail.com>
2018-01-10 23:45:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ function sayHiToGrandma(string) {
       if (string.toUpperCase() === string) {
     return "YES INDEED!" } 
       if (string = "I love you!") {
-        return "I love you, too!"}
+        return "I love you, too."}
 }
